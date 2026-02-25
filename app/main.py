ae7bf364cff069d9b11c4dd5abde2797eb64d319
Gazala Parveen <gazala.parveen@carrier.com>
2026-02-25 12:13:35 +0530

    main.py changes

--- app/main.py
+++ app/main.py
@@ -1,23 +1,31 @@
-from fastapi import FastAPI
-from .database import Base, engine
-from .routers import users, roles, projects, tasks
-from fastapi.middleware.cors import CORSMiddleware
-origins = [
-    "http://localhost:5174",
-]
+# from fastapi import FastAPI
+# from .database import Base, engine
+# from .routers import users, roles, projects, tasks
+# from fastapi.middleware.cors import CORSMiddleware
+# origins = [
+#     "http://localhost:5174",
+# ]
 
-Base.metadata.create_all(bind=engine)
+# Base.metadata.create_all(bind=engine)
+
+# app = FastAPI()
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+
+# app.include_router(users.router)
+# app.include_router(roles.router)
+# app.include_router(projects.router)
+# app.include_router(tasks.router)
+
+from fastapi import FastAPI
 
 app = FastAPI()
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
-app.include_router(users.router)
-app.include_router(roles.router)
-app.include_router(projects.router)
-app.include_router(tasks.router)
+@app.get("/")
+def read_root():
+    return {"status": "working"}
